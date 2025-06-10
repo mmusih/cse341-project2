16d1abbc4cac1821c42a49cc6f6773ba09897ccb
mmusih <hub22001@byui.edu>
2025-06-10 14:24:26 +0200

    contact_date

--- middleware/validate.js
+++ middleware/validate.js
@@ -30,7 +30,7 @@ const saveContact = (req, res, next) => {
     lastName: 'required|string',
     email: 'required|email',
     favoriteColor: 'required|string',
-    birthday: 'string'
+    birthday: 'required|date'
   };
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) {
